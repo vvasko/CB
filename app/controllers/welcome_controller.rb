14b7cb8d4d9e052101c6cdf3ccb4156d4dbcea70
Vera <vvasko@spaceotechnologies.ru>
2015-12-04 00:46:46 +0200

    group cocktails by type

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,13 +2,14 @@ class WelcomeController < ApplicationController
   before_filter :find_item, only: [:show]
 
   def index
-    # puts params[:product]
     if params[:product].present?
-      @cocktails = Cocktail.all_with_includes_by_product  params[:product]
-      render :index
+      @cocktails = Cocktail.all_with_includes_by_product params[:product]
     else
       @cocktails = Cocktail.all_with_includes
     end
+
+    @cocktails = filter_by :type
+    @cocktails = @cocktails.group_by{ |c| c.type }
 
   end
 
@@ -20,4 +21,14 @@ class WelcomeController < ApplicationController
   def find_item
     @cocktail= Cocktail.find_with_includes params[:id]
   end
+
+  def filter_by field
+    if params[field].present?
+      @cocktails = @cocktails.select { |v| v.send(field) == params[field] }
+    end
+
+    @cocktails
+  end
+
+
 end
