9b48c4a5c7570936040efa3dc331042c0b902439
uncle.istvan55@gmail.com <7reHDf1>
2015-12-11 18:10:16 +0200

    Changed :new to :pending

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,5 +3,5 @@ class Order < ActiveRecord::Base
   has_many :ordered_cocktails
   belongs_to :table
 
-  enum status: [ :new, :delivered, :payed, :closed]
+  enum status: [ :pending, :delivered, :payed, :closed] #rails wasn't all that trilled about :new, with it being default action, and stuff. So I changed it to :pending
 end
