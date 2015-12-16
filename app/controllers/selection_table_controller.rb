666be681d109caf7a63368dac8ba11d2199ee67f
uncle.istvan55@gmail.com <uncle.istvan55@gmail.com>
2015-12-16 19:04:29 +0200

    change select_table logic to clear useless orders

--- app/controllers/selection_table_controller.rb
+++ app/controllers/selection_table_controller.rb
@@ -6,12 +6,14 @@ class SelectionTableController < ApplicationController
     @current_table = get_current_table_id
     if @current_table.present?
       Table.set_status @current_table, Table.statuses[:free]
+      useless_orders = Order.where(table_id: @current_table)
+      useless_orders.update_all(status: Order.statuses[:closed])
     end
   end
 
   def select
     set_current_table_id params[:id]
-
+    puts "ther should be select <<<<<<<<<<<<<<<<<<<<<<<<<<"
     Table.set_status params[:id], Table.statuses[:occupied]
 
     redirect_to welcome_index_path
